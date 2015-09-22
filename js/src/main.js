11e0165d9129ed735e8bb063f642c925c7bd6b8d
Ashley Keller <ashleykeller@ashashash.local>
2015-09-22 12:51:20 -0400

    Fixing footer classes to align with grid

--- js/src/main.js
+++ js/src/main.js
@@ -1,1 +1,72 @@
- 
+window.addEventListener("load", function () {
+
+    //define constants
+    var GAME_WIDTH = 640;
+    var GAME_HEIGHT = 360;
+
+    //grab the canvas and context
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    //keep the game going
+    var gameLive = true;
+
+    //enemies
+    var enemies = [
+        {
+            x: 100, //x coordinate
+            y: 50, //y coordinate
+            w: 30, //width property
+            h: 30, //height property
+            speedY: 2 //speed in Y
+            },
+        {
+            x: 200, //x coordinate
+            y: 200, //y coordinate
+            w: 30, //width property
+            h: 30, //height property
+            speedY: 2 //speed in Y
+            },
+        {
+            x: 320, //x coordinate
+            y: 100, //y coordinate
+            w: 30, //width property
+            h: 30, //height property
+            speedY: 2 //speed in Y
+            },
+        {
+            x: 450, //x coordinate
+            y: 20, //y coordinate
+            w: 30, //width property
+            h: 30, //height property
+            speedY: 2 //speed in Y
+            }
+        ]
+
+    //update the logic
+    var update = function () {};
+
+    //draw
+    var draw = function () {
+        ctx.clearRect(0, 0, GAME_HEIGHT, GAME_WIDTH);
+        ctx.fillStyle = ("#f00");
+
+        enemies.forEach(function (element, index) {
+            ctx.fillRect(element.x, element.y, element.w, element.h);
+        });
+    };
+
+    //gets executed multiple times per second
+    var step = function () {
+
+        // update();
+        draw();
+
+        if (gameLive) {
+            window.requestAnimationFrame(step);
+        }
+    };
+
+    step();
+
+}); //load listener
