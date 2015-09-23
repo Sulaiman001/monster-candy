8c27f237d6dfd07dcbc0c49bfd536ec9f89d2502
Ashley Keller <ashleyhkeller@gmail.com>
2015-09-22 20:30:32 -0400

    Bouncing enemies off boundaries and adding player element

--- js/dist/concatenated-files.js
+++ js/dist/concatenated-files.js
@@ -14,7 +14,7 @@ window.addEventListener("load", function () {
     //enemies
     var enemies = [
         {
-            x: 130, //x coordinate
+            x: 150, //x coordinate
             y: 50, //y coordinate
             w: 30, //width property
             h: 30, //height property
@@ -22,46 +22,64 @@ window.addEventListener("load", function () {
             },
         {
             x: 250, //x coordinate
-            y: 450, //y coordinate
+            y: 150, //y coordinate
             w: 30, //width property
             h: 30, //height property
             speedY: 2 //speed in Y
             },
         {
             x: 400, //x coordinate
-            y: 450, //y coordinate
+            y: 70, //y coordinate
             w: 30, //width property
             h: 30, //height property
             speedY: -2 //speed in Y
             },
         {
             x:570, //x coordinate
-            y: 100, //y coordinate
+            y: 200, //y coordinate
             w: 30, //width property
             h: 30, //height property
-            speedY: 2 //speed in Y
+            speedY: -2 //speed in Y
             },
         {
             x: 700, //x coordinate
             y: 550, //y coordinate
             w: 30, //width property
             h: 30, //height property
-            speedY: -1 //speed in Y
+            speedY: 1 //speed in Y
             },
         {
             x: 820, //x coordinate
-            y: 550, //y coordinate
+            y: 300, //y coordinate
             w: 30, //width property
             h: 30, //height property
             speedY: 2 //speed in Y
             }
         ]
+    
+    //player
+    var player = {
+            x: 50, //x coordinate
+            y: 300, //y coordinate
+            w: 30, //width property
+            h: 30, //height property
+            speedX: 2 //speed in X
+    }
 
     //update the logic
     var update = function () {
 
         enemies.forEach(function (element, index) {
             element.y += element.speedY;
+            
+            //check for game borders and reverse speed
+            if (element.y <= 10) {
+                element.y = 10;
+                element.speedY *= -1;
+            } else if (element.y >= GAME_HEIGHT - 50) {
+                element.y = GAME_HEIGHT - 50;
+                element.speedY *= -1;
+            }
         });
 
     };
@@ -69,6 +87,10 @@ window.addEventListener("load", function () {
     //draw
     var draw = function () {
         ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        
+        ctx.fillStyle = ("#6960FF");
+        ctx.fillRect(player.x, player.y, player.w, player.h);
+        
         ctx.fillStyle = ("#1C2326");
 
         enemies.forEach(function (element, index) {
