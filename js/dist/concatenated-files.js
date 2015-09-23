4e354457c558825f27ff37b6ad413a7b72b90092
Ashley Keller <ashleykeller@ashashash.local>
2015-09-23 08:44:21 -0400

    Resolving collision error between enemies and player

--- js/dist/concatenated-files.js
+++ js/dist/concatenated-files.js
@@ -4,9 +4,9 @@ window.addEventListener("load", function () {
     var GAME_WIDTH = 960;
     var GAME_HEIGHT = 640;
 
-    var enemyWidth = 65;
-    var enemyHeight = 65;
-    
+    var enemyWidth = 70;
+    var enemyHeight = 69;
+
     //enemies
     var enemies = [
         {
@@ -31,7 +31,7 @@ window.addEventListener("load", function () {
             speedY: -2 //speed in Y
             },
         {
-            x:520, //x coordinate
+            x: 520, //x coordinate
             y: 200, //y coordinate
             w: enemyWidth, //width property
             h: enemyHeight, //height property
@@ -42,7 +42,7 @@ window.addEventListener("load", function () {
             y: 550, //y coordinate
             w: enemyWidth, //width property
             h: enemyHeight, //height property
-            speedY: 2 //speed in Y
+            speedY: 1 //speed in Y
             },
         {
             x: 760, //x coordinate
@@ -52,99 +52,99 @@ window.addEventListener("load", function () {
             speedY: 2 //speed in Y
             }
         ]
-    
+
     //player
     var player = {
-            x: 50, //x coordinate
-            y: 300, //y coordinate
-            w: 50, //width property
-            h: 50, //height property
-            speedX: 2, //speed in X
-            isMoving: false
+        x: 50, //x coordinate
+        y: 300, //y coordinate
+        w: 70, //width property
+        h: 69, //height property
+        speedX: 3, //speed in X
+        isMoving: false
     }
-    
+
     //goal
     var goal = {
-            x: 880, //x coordinate
-            y: 300, //y coordinate
-            w: 50, //width property
-            h: 50, //height property
+        x: 880, //x coordinate
+        y: 300, //y coordinate
+        w: 50, //width property
+        h: 50, //height property
     }
-    
+
     //set levels
     var level = 1;
-    
+
     //keep the game going
     var gameLive = true;
-    
+
     //basic player movement setup
-    var movePlayer = function() {
+    var movePlayer = function () {
         player.isMoving = true;
     };
-    
-    var stopPlayer = function() {
+
+    var stopPlayer = function () {
         player.isMoving = false;
     };
-    
+
     //grab the canvas and context
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
-    
+
     //event listeners for interaction
     canvas.addEventListener("mousedown", movePlayer);
     canvas.addEventListener("mouseup", stopPlayer);
     canvas.addEventListener('touchstart', movePlayer);
     canvas.addEventListener('touchend', stopPlayer);
-    
+
     var sprites = {};
-    
-    var load = function() {
-            sprites.enemy = new Image();
-            sprites.enemy.src = "images/evil-candy.png";
+
+    var load = function () {
+        sprites.enemy = new Image();
+        sprites.enemy.src = "images/evil-candy.png";
     };
 
     //update the logic
     var update = function () {
-        
+
         //check if you've won the game
-        
-        if(checkCollision(player, goal)) {
-            
+
+        if (checkCollision(player, goal)) {
+
             //increase level
             level++;
-            
+
             //set player back to the start
             player.x = 50;
-            
+
             //increase the speed of the enemies by 1
             enemies.forEach(function (element, index) {
                 element.speedY += element.speedY / Math.abs(element.speedY);
             });
-            
+
         };
-        
+
         //player speed
-        if(player.isMoving) {
-            player.x = player.x + player.speedX 
+        if (player.isMoving) {
+            player.x = player.x + player.speedX
         };
-        
+
         //start enemies moving
         enemies.forEach(function (element, index) {
-            
+
             //check for collision
-            if(checkCollision(player, element)) {
+            if (checkCollision(player, element)) {
                 //stop the game
+
                 gameLive = false;
-                
-                alert("game over!");
-                
-                //reload page
+
+                alert('Game over!');
+
                 window.location = "";
             };
-            
+
             //enemy speed
             element.y += element.speedY;
-            
+
             //check for game borders and reverse speed
             if (element.y <= 10) {
                 element.y = 10;
@@ -160,13 +160,13 @@ window.addEventListener("load", function () {
     //draw
     var draw = function () {
         ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-        
+
         ctx.fillStyle = ("#6960FF");
         ctx.fillRect(player.x, player.y, player.w, player.h);
-        
+
         ctx.fillStyle = ("#FFC763");
         ctx.fillRect(goal.x, goal.y, goal.w, goal.h);
-        
+
         ctx.fillStyle = ("#1C2326");
 
         enemies.forEach(function (element, index) {
@@ -184,14 +184,16 @@ window.addEventListener("load", function () {
             window.requestAnimationFrame(step);
         }
     };
-    
-    
+
+
     //check the collision between two rectangles
     var checkCollision = function (rect1, rect2) {
 
         var closeOnWidth = Math.abs(rect1.x - rect2.x) <= Math.max(rect1.w, rect2.w);
         var closeOnHeight = Math.abs(rect1.y - rect2.y) <= Math.max(rect1.h, rect2.h);
         return closeOnWidth && closeOnHeight;
+
+
     };
 
     //initial kick
